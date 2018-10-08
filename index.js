e81ffece2ce19516e3f5939f9139b5566ed089f9
Anandha Sudhan <rasudhan@gmail.com>
2018-10-08 20:07:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function Dog(name,breed,age) {
+  this.name=name
+  this.breed=breed
+  this.age=age
+}
+
+function Cat(name,breed,age) {
+  this.name=name
+  this.breed=breed
+  this.age=age
+}
+
+function Ferret(name,age,foodRequirements) {
+  this.name=name
+  this.foodRequirements=foodRequirements
+  this.age=age
+}
+
+function Bird(name,breed){
+  this.name=name
+  
+  this.age=age
+}
